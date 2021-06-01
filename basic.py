ee92bb09a8b9a2a68d5e7b1bf8a166f820ee3842
danielq1985 <57229989+danielq1985@users.noreply.github.com>
2021-05-31 20:50:52 -0600

    Add files via upload

--- basic.py
+++ basic.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 
 from itertools import chain
+import folium
 
 
 app = Flask(__name__)
@@ -47,6 +48,7 @@ def book_chapters(api, b_id):
 
     for item in response_json["data"]:
         array.append(item["id"])
+
     return array
 
 
@@ -64,6 +66,13 @@ def book_chapter_content(api, b_id):
 
 
 def book_chapter_content_esv(b_id):
+    # Ruth and Joel are the only two books that the api book id doesnt match the ESV api format. Instead of
+    # changing EVERYTHING this is a bandaid
+    if 'RUT' in b_id:
+        b_id = b_id.replace("RUT", "RUTH")
+    elif 'JOL' in b_id:
+        b_id = b_id.replace("JOL", "JOEL")
+
     url = "https://api.esv.org/v3/passage/text/?q={}".format(b_id)
 
     payload = {}
@@ -128,7 +137,7 @@ def search_bible(api, search_string):
 
 def cross(chapter_input):
     df = pd.read_csv("static/cross.csv")
-    selected_chapter = df[(df.book_chapter == '{}'.format(chapter_input))][['verse', 'to_verse', 'to_verse_display']]
+    selected_chapter = df[(df.book_chapter == '{}'.format(chapter_input))][['verse', 'to_verse', 'to_verse_display', 'book_chapter']]
     arr = selected_chapter.verse.unique()
     master_array = []
     for item in arr:
@@ -142,9 +151,15 @@ def cross(chapter_input):
         for item3 in b:
             b_arr.append(item3)
 
+        c = selected_chapter[(selected_chapter.verse == item)]['book_chapter']
+        c_arr = []
+        for item4 in c:
+            c_arr.append(item4)
+
         dictionary = dict(zip(a_arr, b_arr))
 
-        case = {"Verse": item, "Verses": dictionary}
+        case = {"Verse": item, "Verses": dictionary, "book_chapter": c_arr}
+        # print(case)
         master_array.append(case)
 
     if master_array:
@@ -157,6 +172,8 @@ def ppt(book_chapter):
     df = pd.read_csv("static/verses.csv")
     df_people = pd.read_csv("static/people.csv")
     df_places = pd.read_csv("static/places.csv")
+    df_books = pd.read_csv("static/books.csv")
+    df_chapters = pd.read_csv("static/chapters.csv")
 
     main = df[(df.book_chapter == '{}'.format(book_chapter))]
     main = main.fillna('0')
@@ -203,70 +220,64 @@ def ppt(book_chapter):
 
     dictionary_places = dict(zip(places_list_v, places_list_k))
 
-    case = {"people": dictionary_people, "places": dictionary_places}
+###########################################################
+
+    book_key = book_chapter.split('.')
+    book_key = book_key[0]
+
+    book_author = df_books[(df_books.osisName==book_key)].writers
+    for item in book_author:
+        book_author_arr = item.split(", ")
+
+    author_list_k = []
+    for item in book_author_arr:
+        name_display = df_people[(df_people.personLookup == "{}".format(item))]["displayTitle"].iat[0]
+        author_list_k.append(name_display)
+
+    book_authors_dict = dict(zip(book_author_arr, author_list_k))
+
+############################################################
+
+    if 'intro' in book_chapter:
+        book_chapter1 = book_key + '.' + '1'
+    else:
+        book_chapter1 = book_chapter
+
+    book_chapter_author = df_chapters[(df_chapters.book_chapter==book_chapter1)].writer.iat[0]
+
+    chapter_author_arr = []
+    chapter_author_list_k = []
+    if type(book_chapter_author) == float:
+        pass
+    else:
+        book_chapter_author = df_chapters[(df_chapters.book_chapter == book_chapter1)].writer
+        for item in book_chapter_author:
+            chapter_author_arr = item.split(", ")
+
+        chapter_author_list_k = []
+        for item in chapter_author_arr:
+            name_display = df_people[(df_people.personLookup == "{}".format(item))]["displayTitle"].iat[0]
+            chapter_author_list_k.append(name_display)
+
+    chapter_authors_dict = dict(zip(chapter_author_arr, chapter_author_list_k))
+    # print(chapter_authors_dict)
+
+##############################################################################
+
+    book_name = df_books[(df_books.osisName == book_key)].bookName.iat[0]
+    book_div = df_books[(df_books.osisName == book_key)].bookDiv.iat[0]
+    book_testament = df_books[(df_books.osisName == book_key)].testament.iat[0]
+
+    book_info = "<b>Book:</b> {} <br><br> <b>BookDiv:</b> {} <br><br> <b>Testament:</b> {} <br><br>".format(book_name, book_div, book_testament)
+
+    events_timeline = df[(df.book_chapter == '{}'.format(book_chapter))].eventsDescribed.iat[0]
+
+    print(book_name, book_div, book_testament)
+
+    case = {"people": dictionary_people, "places": dictionary_places, "book_authors": book_authors_dict,
+            "chapter_authors": chapter_authors_dict, "book_info": book_info}
 
     return case
-
-
-    # df_chapter = df[(df.book_chapter == "{}".format(book_chapter))][
-    #     ["verseNum", "people", "places", "yearNum", "eventsDescribed", "timeline"]]
-    # df_chapter = df_chapter.fillna('0')
-    #
-    # arr = df_chapter[(df_chapter.people != '0') | (df_chapter.places != '0')]
-    # arr_l = arr['verseNum']
-    #
-    # array = []
-    #
-    # for item in arr_l:
-    #     people_data = arr[(arr.verseNum == item)]['people']
-    #     places_data = arr[(arr.verseNum == item)]['places']
-    #
-    #     ## people ##
-    #     for item2 in people_data:
-    #         people = item2.split(",")
-    #
-    #     array_name_disp = []
-    #     array_name = []
-    #     if '0' not in people:
-    #         for item2a in people:
-    #             name_display = df_people[(df_people.personLookup == "{}".format(item2a))]["displayTitle"].iat[0]
-    #             array_name_disp.append(name_display)
-    #
-    #         for item2a in people:
-    #             name = item2a
-    #             array_name.append(name)
-    #
-    #     else:
-    #         people = '0'
-    #         array_name_disp.append(people)
-    #         array_name.append(people)
-    #     dictionary_people = dict(zip(array_name_disp, array_name))
-    #
-    #     ## places ##
-    #     for item3 in places_data:
-    #         places = item3.split(",")
-    #
-    #     array_places_disp = []
-    #     array_places = []
-    #     if '0' not in places:
-    #         for item3a in places:
-    #             places_display = df_places[(df_places.placeLookup == "{}".format(item3a))]["displayTitle"].iat[0]
-    #             array_places_disp.append(places_display)
-    #
-    #         for item3a in places:
-    #             place = item3a
-    #             array_places.append(place)
-    #
-    #     else:
-    #         places = '0'
-    #         array_places_disp.append(places)
-    #         array_places.append(places)
-    #
-    #     dictionary_places = dict(zip(array_places_disp, array_places))
-    #
-    #     case = {"verse": item, "people": dictionary_people, "places": dictionary_places}
-    #     array.append(case)
-    # return array
 
 
 # # # # # Classes # # # # #
@@ -413,48 +424,186 @@ def search():
 @app.route('/ref/<ref_input>')
 def ref(ref_input):
 
+    split2 = ref_input.split('.', -1)
+    chapter = "{}.{}".format(split2[0], split2[1])
+    goto_chapter = """
+    <a class="btn btn-outline-secondary btn-sm" id=loadingMain href="/verse?verse_data={}">Chapter →</a>
+    """.format(chapter)
+
     a = book_chapter_content_esv(ref_input)
+
     b = a['query']
     for item in a['passages']:
         c = item
 
-    return jsonify(ref=b, text=c)
+    return jsonify(ref=b, text=c, goto_chapter=goto_chapter)
+
+
+@app.route('/ref_modal/<ref_input>')
+def ref_modal(ref_input):
+
+    book_list1 = ['Gen', 'Exod', 'Lev', 'Num', 'Deut', 'Josh', 'Judg', 'Ruth',
+       '1Sam', '2Sam', '1Kgs', '2Kgs', '1Chr', '2Chr', 'Ezra', 'Neh',
+       'Esth', 'Job', 'Ps', 'Prov', 'Eccl', 'Song', 'Isa', 'Jer', 'Lam',
+       'Ezek', 'Dan', 'Hos', 'Joel', 'Amos', 'Obad', 'Jonah', 'Mic',
+       'Nah', 'Hab', 'Zeph', 'Hag', 'Zech', 'Mal', 'Matt', 'Mark', 'Luke',
+       'John', 'Acts', 'Rom', '1Cor', '2Cor', 'Gal', 'Eph', 'Phil', 'Col',
+       '1Thess', '2Thess', '1Tim', '2Tim', 'Titus', 'Phlm', 'Heb', 'Jas',
+       '1Pet', '2Pet', '1John', '2John', '3John', 'Jude', 'Rev']
+
+    book_list = ['GEN', 'EXO', 'LEV', 'NUM', 'DEU', 'JOS', 'JDG', 'RUT', '1SA',
+                 '2SA', '1KI', '2KI', '1CH', '2CH', 'EZR', 'NEH', 'EST', 'JOB',
+                 'PSA', 'PRO', 'ECC', 'SNG', 'ISA', 'JER', 'LAM', 'EZK', 'DAN',
+                 'HOS', 'JOL', 'AMO', 'OBA', 'JON', 'MIC', 'NAM', 'HAB', 'ZEP',
+                 'HAG', 'ZEC', 'MAL', 'MAT', 'MRK', 'LUK', 'JHN', 'ACT', 'ROM',
+                 '1CO', '2CO', 'GAL', 'EPH', 'PHP', 'COL', '1TH', '2TH', '1TI',
+                 '2TI', 'TIT', 'PHM', 'HEB', 'JAS', '1PE', '2PE', '1JN', '2JN',
+                 '3JN', 'JUD', 'REV']
+    goto_chapter = ''
+    count = -1
+    for item in range(66):
+        count += 1
+
+        if book_list1[count] in ref_input:
+            split = ref_input.split('.', -1)
+            chapter = "{}.{}".format(book_list[count], split[1])
+
+            goto_chapter = """
+            <a class="btn btn-outline-secondary btn-sm" id=loadingMainModal href="/verse?verse_data={}">Chapter →</a>
+            """.format(chapter)
+
+    a = book_chapter_content_esv(ref_input)
+
+    b = a['query']
+    for item in a['passages']:
+        c = item
+
+    return jsonify(ref=b, text=c, goto_chapter=goto_chapter)
 
 
 @app.route('/person/<person>')
 def person(person):
     df_people = pd.read_csv("static/people.csv")
+    df_people = df_people.applymap(str)
+
+    df_places = pd.read_csv("static/places.csv")
+    df_places = df_places.applymap(str)
+
     df = df_people[(df_people.personLookup == '{}'.format(person))]
-    # df = df.fillna('0')
 
     display = df['displayTitle'].iat[0]
 
-    aka = df['alsoCalled'].iat[0]
-    akaDis = ''
-    if aka is not np.nan:
-        akaDis = '<br> Also known as: {} <br>'.format(aka)
-
-    dictText1 = df['dictText'].iat[0]
-    if dictText1 is not np.nan:
-        dictText = dictText1
-    else:
+    dictText = df['dictText'].iat[0]
+    if dictText is np.nan:
         dictText = ""
 
+    aka = df['alsoCalled'].iat[0]
+    akaDis = ''
+    if aka != 'nan':
+        aka_1 = aka.replace(',', ', ')
+        akaDis = '<br><b> Also known as:</b> {} <br>'.format(aka_1)
+    else:
+        akaDis = ''
+
     verses1 = df['verses']
+    verses_arr = []
     if verses1 is not np.nan:
         for item in verses1:
             verses = item.split(",")
+            # for item2 in verses:
+            #     verses2 = item2.split('.', -1)
+            #     verses3 = verses2[0] + '.' + verses2[1]
+            #     verses_arr.append(verses3)
+
     else:
         verses = ""
 
-    button = []
-    for item in verses:
-        html = """
-            <a class="process_input" href="/ref/{}" role="button">{}</a>
-            """.format(item, item)
-        button.append(html)
+    # print(verses_arr)
 
-    return jsonify(display=display, dictText=dictText, verses=verses, button=button, akaDis=akaDis)
+    birth_year = df['birthYear'].iat[0]
+    birth_year_display = ''
+    birth_year_display1 = str(birth_year)
+    if birth_year_display1 != 'nan':
+        if '-' in birth_year_display1:
+            birth_year_display2 = birth_year_display1[1:]
+            birth_year_display = '<br><b> Birth year:</b> {} BC <br>'.format(birth_year_display2[:-2])
+        else:
+            birth_year_display1 = str(birth_year)
+            birth_year_display2 = birth_year_display1
+            birth_year_display = '<br><b> Birth year:</b> {} AD <br>'.format(birth_year_display2[:-2])
+
+    death_year = df['deathYear'].iat[0]
+    death_year_display = ''
+    death_year_display1 = str(death_year)
+    if death_year_display1 != 'nan':
+        if '-' in death_year_display1:
+            death_year_display2 = death_year_display1[1:]
+            death_year_display = '<br><b> Death year:</b> {} BC <br>'.format(death_year_display2[:-2])
+        else:
+            death_year_display1 = str(death_year)
+            death_year_display2 = death_year_display1
+            death_year_display = '<br><b> Death year:</b> {} AD <br>'.format(death_year_display2[:-2])
+
+    birth_place = df['birthPlace'].iat[0]
+    birth_place_display = ''
+    if birth_place != 'nan':
+        birth_place_display1 = df_places[(df_places.placeLookup == "{}".format(birth_place))]["displayTitle"].iat[0]
+        birth_place_display = "<br><b> Birth Place:</b> {} <br>".format(birth_place_display1)
+
+    death_place = df['deathPlace'].iat[0]
+    death_place_display = ''
+    if death_place != 'nan':
+        death_place_display1 = df_places[(df_places.placeLookup == "{}".format(death_place))]["displayTitle"]
+        death_place_display = "<br><b> Death Place:</b> {} <br>".format(death_place_display1)
+
+    modal_arr = []
+
+    for item in verses:
+
+        modal = """
+        
+    
+              <!-- Trigger the modal with a button -->
+              <a type="button" class="process_input_verse_modal" id={} data-toggle="modal" data-target="#myModal">{}</a>
+            
+              <!-- Modal -->
+              <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
+                
+                  <!-- Modal content-->
+                  <div class="modal-content">
+                    <div class="modal-header" >
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div id="modal-body" style="margin:40px">
+                        <div id='loadingmessageModal' style='display:none'>
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden"></span>
+                        </div>
+                        
+        </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+              
+          
+              
+            """.format(item, item)
+        # <a class="process_input" id="{}" back_key="{}" data-toggle="collapse" href="#" role="button">{}</a>
+
+        modal_arr.append(modal)
+
+    test = df.father.iat[0]
+    print(test)
+
+    return jsonify(display=display, dictText=dictText, verses=verses, akaDis=akaDis,
+                   birth_year_display=birth_year_display, death_year_display=death_year_display, birth_place_display=birth_place_display,
+                   death_place_display=death_place_display, modal=modal_arr)
 
 
 @app.route('/place/<place>')
@@ -478,8 +627,99 @@ def places(place):
     else:
         verses = ""
 
-    return jsonify(display=display, dictText=dictText, verses=verses)
+    modal_arr = []
+    for item in verses:
+        modal = """
 
+
+                  <!-- Trigger the modal with a button -->
+                  <a type="button" class="process_input_verse_modal" id={} data-toggle="modal" data-target="#myModal">{}</a>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="myModal" role="dialog">
+                    <div class="modal-dialog">
+
+                      <!-- Modal content-->
+                      <div class="modal-content">
+                        <div class="modal-header" >
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div id="modal-body" style="margin:40px">
+                        <div id='loadingmessageModal' style='display:none'>
+    <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden"></span>
+    </div>
+</div>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+
+
+                """.format(item, item)
+        # <a class="process_input" id="{}" back_key="{}" data-toggle="collapse" href="#" role="button">{}</a>
+
+        modal_arr.append(modal)
+
+    lat = df['openBibleLat'].iat[0]
+    long = df['openBibleLong'].iat[0]
+
+    # print(lat, long)
+    # print(type(lat), type(long))
+
+    if lat == '0':
+        map = ""
+    else:
+        map = """
+        
+                  <!-- Trigger the modal with a button -->
+                  <a class="btn btn-outline-secondary btn-sm" type="button" data-toggle="modal" data-target="#myModalmap">map →</a>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="myModalmap" role="dialog">
+                    <div class="modal-dialog modal-lg">
+
+                      <!-- Modal content-->
+                      <div class="modal-content">
+                        <div class="modal-header" >
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div id="modal-body-map" class="container">
+                                <center>
+                                <h4 class="modal-title"> {} </h4>
+                                <iframe 
+                                src="/map/{}/{}/{}" frameborder="0" src="" style="position: relative; height: 450px; width: 100%;">
+                                </iframe>
+                                </center>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+        
+        """.format(display, lat, long, display)
+
+    # print(lat, long)
+
+    return jsonify(display=display, dictText=dictText, modal=modal_arr, map=map)
+
+
+@app.route('/map/<lat>/<long>/<name>/')
+def map(lat, long, name):
+    cords = (lat, long)
+    folium_map = folium.Map(location=cords, tiles='Stamen Terrain')
+    folium.Marker(
+        [lat, long], popup="<i>{}</i>".format(name),
+    ).add_to(folium_map)
+    return folium_map._repr_html_()
 
 
 if __name__ == '__main__':
